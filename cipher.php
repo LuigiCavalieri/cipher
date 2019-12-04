4e794e7f0e17ca919fa326775429dd53b8f95235
LuigiCavalieri <58509573+LuigiCavalieri@users.noreply.github.com>
2019-12-04 11:20:06 +0100

    Starting repository with Cipher 1.2

---
+++ cipher.php
@@ -0,0 +1,48 @@
+<?php 
+/**
+ * Plugin Name: Cipher
+ * Plugin URI: https://sitetreeplugin.com/blog/cipher/
+ * Description: Cipher allows commenters to publish (pre-formatted) code.
+ * Version: 1.2
+ * Author: Luigi Cavalieri
+ * Author URI: http://luigicavalieri.com
+ * License: GPL v3.0
+ * License URI: https://opensource.org/licenses/GPL-3.0
+ * 
+ * 
+ * @package Cipher
+ * @version 1.2
+ * @copyright Copyright 2019 Luigi Cavalieri.
+ * @license https://opensource.org/licenses/GPL-3.0 GPL v3.0
+ * 
+ * 
+ * Copyright 2019 Luigi Cavalieri.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * ************************************************************************* */
+
+
+if (! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if (! class_exists( 'LC_Plugin' ) ) {
+	include( 'library/plugin.class.php' );
+}
+
+include( 'core/cipher.class.php' );
+
+Cipher::launch( __FILE__ );
+?>
